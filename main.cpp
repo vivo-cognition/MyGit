c5142e12bdedfff8e1aefaed1006187097c4b7e3
vivo-cognition <olgamilisuk020@gmail.com>
2026-02-16 16:58:48 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string text= "hello";
+	int textLength = text.length();
+	string word;
+	cin >> word;
+	int wordLength = word.length();
+	int j = 0, i=0;
+	while (j<textLength && i<wordLength) {
+    	if (word[i] == text[j]) {		
+		j++;
+		}
+	 i++;
+	}
+	if (j == textLength)
+	{
+		cout << "YES";
+	}
+	else
+	{
+		cout << "NO";
+	}
+	return 0;
+}
