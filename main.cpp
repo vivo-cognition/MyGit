7493ed8b4181f921dce6f187c5b81681e6852777
vivo-cognition <olgamilisuk020@gmail.com>
2026-02-16 17:35:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <string>
 using namespace std;
 
-}
+bool proverka(string word) {
+	if (word == "dsheflldfo") {
+		return true;
+	}
+	if (word == "sdfergr") {
+		return false;
+	}
 int main() {
 	string text= "hello";
 	int textLength = text.length();
