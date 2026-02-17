b8a6dfff0852f8fcf8f7de9bf08335e43886a12c
vivo-cognition <olgamilisuk020@gmail.com>
2026-02-17 10:22:24 +0300

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string proverka (int number, int arr[]) {
+	for (int i = 0; i < 14;i++) {
+		if (number % arr[i] == 0) {
+			return "YES";
+		}
+	}
+	return "NO";
+}
+
+int main() {
+	int number;
+	cin >> number;
+    int arr[] = {7,4,44, 77,47,74,444,447,474,477,744,747,774,777};
+	cout << proverka(number,arr);
+	return 0;
+}
